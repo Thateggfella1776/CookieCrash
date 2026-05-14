a3788d0fb6b9e0b899570e0a08f3d9802fdfc671
Thateggfella1776 <gavin.allen@ht-la.org>
2026-05-13 21:18:23 -0700

    Fixed UI so its not so sloppy, added auto click upgrade, began creation of save/load components

--- src/Cookies.java
+++ src/Cookies.java
@@ -1,16 +1,42 @@
 public class Cookies {
     int cookieCount = 0;
     int CookiesPerClick = 1;
+    int AutoCookies = 0;
     int Upgrade1Purchases = 0;
     int Upgrade1BaseCost = 15;
+    int Upgrade2Purchases = 0; //First of the "auto clickers" for now possibly will be moved to buildings
+    int Upgrade2BaseCost = 100;
 
     public int getCookieCount(){return cookieCount;}
     public int getCookiesPerClick(){return CookiesPerClick;}
+    public int getAutoCookies(){return AutoCookies;}
     public int getUpgrade1Purchases(){return Upgrade1Purchases;}
+    public int getUpgrade2Purchases(){return Upgrade2Purchases;}
 
     public void Click(){
-        cookieCount+= CookiesPerClick;
+        cookieCount+=CookiesPerClick;
     }
+
+    public void AutoClick(){
+        cookieCount+=AutoCookies;
+    }
+
+    public int getUpgrade2Cost(){
+        //15(base cost)*(1.15)^(times purchased)
+        return (int) Math.round(Upgrade2BaseCost*Math.pow(1.15,Upgrade2Purchases));
+    }
+
+    public boolean buyUpgrade2(){
+        int cost = getUpgrade2Cost();
+        if(cookieCount>=cost){
+            cookieCount-=cost;
+            Upgrade2Purchases++;
+            AutoCookies++;
+            return true;
+        }
+        return false;
+    }
+
 
     public int getUpgrade1Cost(){
         //15(base cost)*(1.15)^(times purchased)
--- src/GUI.java
+++ src/GUI.java
@@ -5,63 +5,159 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener {
+    Timer updateTimer;
+    Timer CPSTimer;
+    ActionListener guiUpdate;
+    ActionListener CPSTimerUpdate;
     Cookies logic = new Cookies();
-    Color myColor = new Color(222, 98, 199);
+    Color myColor = new Color(99, 208, 168);
     JFrame frame;
+    JButton SaveGameButton;
+    JButton LoadGameButton;
     JButton cookieButton;
     JButton Upgrade1Button;
-    JPanel panel;
-    JLabel CountLabel;
+    JButton Upgrade2Button;
+    JPanel MegaPanel = new JPanel();
+    JPanel OptionsPanel;
+    JPanel CookiePanel;
+    JPanel UpgradesPanel;
+    JPanel StatsPanel;
+    JLabel CurrentCookies;
+    JLabel ClickPWR;
+    JLabel AutoPWR;
     ImageIcon Nummie = new ImageIcon("src/PerfectCookie.png");
 
-
     public GUI() {
-        frame = new JFrame("COOKIE CRASH!");
+
+        //GUI Update Timer / Refresh Rate
+        guiUpdate = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ClickPWR.setText("CPC: "+logic.getCookiesPerClick());
+                CurrentCookies.setText("Cookies: "+logic.getCookieCount());
+                AutoPWR.setText("CPS: " +logic.getAutoCookies());
+                if(logic.getAutoCookies()>0){
+                    CPSTimer.start();
+                }
+            }
+        };
+        updateTimer = new Timer(500,guiUpdate);
+        updateTimer.start();
+
+        //Clicks Per Second Timer
+        CPSTimerUpdate = new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                logic.AutoClick();
+            }
+        };
+        CPSTimer = new Timer(1000,CPSTimerUpdate);
+
+
+        //Save & Load Buttons
+        SaveGameButton = new JButton("Save Game");
+        Object[] SaveOptions = {"Yes, please",
+                "No, thanks",};
+        SaveGameButton.addActionListener(e -> {
+            JOptionPane.showOptionDialog(frame,
+                    "Would you like to save your current game data?",
+                    "Save Game?",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    SaveOptions,
+                    SaveOptions[0]);
+        });
+        LoadGameButton = new JButton("Load Game");
+        Object[] LoadOptions = {"Yes, please",
+                "No, thanks",};
+        LoadGameButton.addActionListener(e -> {
+            JOptionPane.showOptionDialog(frame,
+                    "Would you like to load your previous game data?",
+                    "Load Game?",
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    LoadOptions,
+                    LoadOptions[0]);
+        });
+
+        //Cookie Button Creation & Modification
         cookieButton = new JButton(Nummie);
+        cookieButton.setBorderPainted(false);
+        cookieButton.setContentAreaFilled(false);
+        cookieButton.setOpaque(false);
         cookieButton.setPreferredSize(new Dimension(512,512));
         cookieButton.addActionListener(e -> {
             logic.Click();
-            UpdateCountLabel();
+            CurrentCookies.setText("Cookies: "+logic.getCookieCount());
         });
+
+        //Upgrade Buttons
         Upgrade1Button = new JButton("Upgrade (+1 CPC) | Cost: "+logic.getUpgrade1Cost()+" Cookies");
         Upgrade1Button.addActionListener(e -> {
-        if(logic.buyUpgrade1()){
-            UpdateCountLabel();
-            Upgrade1Button.setText("Upgrade (+1 CPC) | Cost: "+logic.getUpgrade1Cost()+" Cookies");
-        }else{
-            JOptionPane.showMessageDialog(frame,
-                    "Not enough cookies! You need " + logic.getUpgrade1Cost() + " cookies.",
-                    "Insufficient Cookies",
-                    JOptionPane.WARNING_MESSAGE); //Source: https://docs.oracle.com/javase/tutorial/uiswing/components/dialog.html
-        }
+            if(logic.buyUpgrade1()==true){
+                Upgrade1Button.setText("Upgrade (+1 CPC) | Cost: "+logic.getUpgrade1Cost()+" Cookies");
+            }else{
+                JOptionPane.showMessageDialog(frame,
+                        "Not enough cookies! You need " + logic.getUpgrade1Cost() + " cookies.",
+                        "Insufficient Cookies",
+                        JOptionPane.WARNING_MESSAGE); //Source: https://docs.oracle.com/javase/tutorial/uiswing/components/dialog.html
+            }
         });
-        CountLabel = new JLabel("Cookies: "+logic.getCookieCount()+" | CPC: "+logic.getCookiesPerClick());//
+        Upgrade2Button = new JButton("Upgrade (+1 CPS) | Cost: "+logic.getUpgrade2Cost()+" Cookies");
+        Upgrade2Button.addActionListener(e -> {
+            if(logic.buyUpgrade2()==true){
+                Upgrade2Button.setText("Upgrade (+1 CPS) | Cost: "+logic.getUpgrade2Cost()+" Cookies");
+            }else{
+                JOptionPane.showMessageDialog(frame,
+                        "Not enough cookies! You need " + logic.getUpgrade2Cost() + " cookies.",
+                        "Insufficient Cookies",
+                        JOptionPane.WARNING_MESSAGE);
+            }
+        });
 
-        panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        //panel.setLayout(new GridLayout(2, 1));
-        panel.setBackground(myColor);
+        //Frame Creation
+        frame = new JFrame("COOKIE CRASH!");
 
-        panel.add(cookieButton);
-        panel.add(Upgrade1Button);
-        panel.add(CountLabel);
+        //Label Creation
+        CurrentCookies = new JLabel("Cookies: "+logic.getCookieCount());
+        ClickPWR = new JLabel("CPC: "+logic.getCookiesPerClick());
+        AutoPWR = new JLabel("CPS: " +logic.getAutoCookies());
 
+        //Panel Creation
+        OptionsPanel = new JPanel();
+        CookiePanel = new JPanel();
+        UpgradesPanel = new JPanel();
+        StatsPanel = new JPanel();
+        MegaPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
+        //Panel Colors
+        MegaPanel.setBackground(myColor);
+        StatsPanel.setBackground(myColor);
+        CookiePanel.setBackground(myColor);
+        UpgradesPanel.setBackground(myColor);
+        OptionsPanel.setBackground(myColor);
 
-        frame.add(panel, BorderLayout.CENTER);
+        //Panel Packing
+        OptionsPanel.add(SaveGameButton);
+        OptionsPanel.add(LoadGameButton);
+        CookiePanel.add(cookieButton);
+        UpgradesPanel.add(Upgrade1Button);
+        UpgradesPanel.add(Upgrade2Button);
+        StatsPanel.add(CurrentCookies);
+        StatsPanel.add(ClickPWR);
+        StatsPanel.add(AutoPWR);
+        MegaPanel.add(StatsPanel);
+        MegaPanel.add(CookiePanel);
+
+        //Frame Packing
+        frame.add(MegaPanel, BorderLayout.CENTER);
+        frame.add(OptionsPanel, BorderLayout.NORTH);
+        frame.add(UpgradesPanel, BorderLayout.EAST);
         frame.setIconImage(Nummie.getImage());
-
         frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frame.setVisible(true);
-
     }
-
-private void UpdateCountLabel(){
-        CountLabel.setText("Cookies: "+logic.getCookieCount()+" | CPC: "+logic.getCookiesPerClick());
-}
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
