4701e4300d580a60c3ce0076af6069a74aea6411
Thateggfella1776 <gavin.allen@ht-la.org>
2026-05-30 10:54:25 -0700

    more upgrade fixing bugs

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
     public static void main(String[] args) {
         new GUI();
         Buildings test = new Buildings();
-        System.out.println(test.getYouCost());
-        System.out.println(test.getCortexBakerCost());
-        System.out.println(test.getIdleverseCost());
-        System.out.println(test.getJavaConsoleCost());
+
     }
 }
